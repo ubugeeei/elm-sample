67184d1b258999ae3f2e76a3a49df2d365e0626e
Ubugeeei <ubuge1122@gamil.com>
2022-07-21 02:02:05 +0900

    feat: impl todo sample

--- src/Main.elm
+++ src/Main.elm
@@ -1,47 +1,105 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, form, input, li, span, text, ul)
+import Html.Attributes exposing (style, type_, value)
+import Html.Events exposing (onClick, onInput)
+import Time exposing (now, toMillis, utc)
 
 
 
 -- MAIN
+
+
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
 
 
 -- MODEL
+
+
+type alias Todo =
+    { title : String
+    , completed : Bool
+    }
+
+
+type alias TodoCreateInput =
+    { title : String
+    }
+
+
 type alias Model =
-    Int
+    { todos : List Todo
+    , todoCreateInput : TodoCreateInput
+    }
+
+
 init : Model
 init =
-    0
+    { todos = []
+    , todoCreateInput = { title = "" }
+    }
 
 
 
 -- UPDATE
+
+
 type Msg
-    = Increment
-    | Decrement
+    = CreateTodo
+    | DeleteTodo Int
+    | HandleInput String
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        CreateTodo ->
+            { model | todos = createTodo model.todos model.todoCreateInput, todoCreateInput = { title = "" } }
 
-        Decrement ->
-            model - 1
+        DeleteTodo index ->
+            { model | todos = deleteTodo model.todos index }
+
+        HandleInput title ->
+            { model | todoCreateInput = { title = title } }
+
+
+createTodo todos todoCreateInput =
+    todos ++ [ { title = todoCreateInput.title, completed = False } ]
+
+
+deleteTodo todos index =
+    List.take index todos ++ List.drop (index + 1) todos
 
 
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ form []
+            [ input [ value model.todoCreateInput.title, onInput HandleInput ] []
+            , button [ type_ "button", onClick CreateTodo ] [ text "CreateTodo" ]
+            ]
+        , todosView model.todos
+        ]
+
+
+todosView : List Todo -> Html Msg
+todosView todos =
+    ul [] (List.indexedMap todoView todos)
+
+
+todoView : Int -> Todo -> Html Msg
+todoView index todo =
+    li []
+        [ input [ type_ "checkbox" ] []
+        , span []
+            [ text todo.title
+            , button [ type_ "button", onClick (DeleteTodo index) ] [ text "Delete" ]
+            ]
         ]
